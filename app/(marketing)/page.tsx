a649a989c2302d8f491ba9d4896cccdf9d6ef5ac
Jan Bojko <honzabojko@seznam.cz>
2024-10-14 23:57:51 +0200

    auth

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -16,21 +16,23 @@ const textFont = Poppins({
 
 const MarketingPage = () => {
 	return (
-		<div className="flex items-center justify-center flex-col">
+		<div className='flex items-center justify-center flex-col'>
 			<div
 				className={cn(
 					"flex items-center justify-center flex-col",
 					headingFont.className
 				)}
 			>
-				<div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
-					<Medal className="h-6 w-6 mr-2" />
-					No 1 task management
+				<div className='mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase'>
+					<Medal className='h-6 w-6 mr-2' />
+					<span className='pt-1 tracking-wide'>
+						No 1 task management
+					</span>
 				</div>
-				<h1 className="text-3xl md:text-6xl text-center text-neutral-800 mb-5">
+				<h1 className='text-3xl md:text-6xl text-center text-neutral-800 mb-5'>
 					Taskify helps team move
 				</h1>
-				<div className="text-3xl md:text-6xl bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md  w-fit">
+				<div className='text-3xl md:text-6xl bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md  w-fit'>
 					work forward
 				</div>
 			</div>
@@ -46,8 +48,8 @@ const MarketingPage = () => {
 				unique - accomplis it all with Taskify
 			</div>
 
-			<Button className="mt-6" size="lg" asChild>
-				<Link href="/sign-up">Get Taskify for free</Link>
+			<Button className='mt-6' size='lg' asChild>
+				<Link href='/sign-up'>Get Taskify for free</Link>
 			</Button>
 		</div>
 	);
