87a55820608b6ac316438d1d807c66b353426cdb
Jan Bojko <honzabojko@seznam.cz>
2024-10-15 11:42:45 +0200

    Organizations, middleware update, --turbo option added for development, navbar component for platform

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,45 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import {
+	clerkMiddleware,
+	createRouteMatcher,
+	redirectToSignIn,
+} from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", "/"]);
 
 export default clerkMiddleware((auth, request) => {
 	if (!isPublicRoute(request)) {
 		auth().protect();
+	}
+
+	if (auth().userId && isPublicRoute(request)) {
+		// Pokud je user přihlášený a chce se dostat na landing page:
+		// Přesměruj ho na výběr organizace
+		let path = "/select-org";
+
+		if (auth().orgId) {
+			// Pokud je user přihlášený a má vybranou organizaci:
+			// Přesměruj ho na jeho organizaci
+			path = `/organization/${auth().orgId}`;
+		}
+
+		const orgSelection = new URL(path, request.url);
+		return NextResponse.redirect(orgSelection);
+	}
+
+	// User není přihlášený a chce se dostat na protected routu
+	if (!auth() && !isPublicRoute(request)) {
+		return auth().redirectToSignIn({ returnBackUrl: request.url });
+	}
+
+	// User je přihlášený a nemá vybranou organizaci a není na routě pro výběr organizace (/select-org)
+	if (
+		auth().userId &&
+		!auth().orgId &&
+		request.nextUrl.pathname !== "/select-org"
+	) {
+		const orgSelection = new URL("/select-org", request.url);
+		return NextResponse.redirect(orgSelection);
 	}
 });
 
