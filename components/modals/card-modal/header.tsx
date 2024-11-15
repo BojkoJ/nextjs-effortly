a911d72c7170dea044b8835a3925ec99cba227cd
Jan Bojko <honzabojko@seznam.cz>
2024-11-15 20:32:19 +0100

    Card description + Card actions (copy,delete)

--- components/modals/card-modal/header.tsx
+++ components/modals/card-modal/header.tsx
@@ -8,7 +8,7 @@ import { CardWithList } from "@/types/lists";
 import { useQueryClient } from "@tanstack/react-query";
 import { Layout } from "lucide-react";
 import { useParams } from "next/navigation";
-import { ElementRef, useRef, useState } from "react";
+import { ElementRef, useRef, useState, useEffect } from "react";
 import { toast } from "sonner";
 
 interface HeaderProps {
@@ -36,6 +36,8 @@ export const Header = ({ data }: HeaderProps) => {
 
 	const inputRef = useRef<ElementRef<"input">>(null);
 
+	const [title, setTitle] = useState(data.title);
+
 	const onBlur = () => {
 		inputRef.current?.form?.requestSubmit();
 	};
@@ -51,8 +53,6 @@ export const Header = ({ data }: HeaderProps) => {
 		execute({ title, boardId, id: data.id });
 	};
 
-	const [title, setTitle] = useState(data.title);
-
 	return (
 		<div className="flex items-start gap-x-3 mb-6 w-full">
 			<Layout className="w-5 h-5 mt-1 text-neutral-700" />
@@ -63,7 +63,7 @@ export const Header = ({ data }: HeaderProps) => {
 						onBlur={onBlur}
 						id="title"
 						defaultValue={title}
-						className="font-semibold text-xl px-1 text-neutral-700 bg-transparent border-transparent relative -left-1.5 w-[95%] focus-visible:bg-white focus-visible:border-input mb-0.5 truncate"
+						className="relative -left-1.5 mb-0.5 w-[95%] truncate border-transparent bg-transparent px-1 text-xl font-semibold text-neutral-700 focus-visible:border-input focus-visible:bg-white"
 					/>
 				</form>
 				<p className="text-sm text-muted-foreground">
